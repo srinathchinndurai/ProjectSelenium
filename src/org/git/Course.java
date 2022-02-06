dd13cf520355fd1e35759059145bc9b3ab2540af
srinathchinnadurai <srinathchinnadurai@gmail.com>
2022-02-06 22:57:19 +0530

    Task done

---
+++ src/org/git/Course.java
@@ -0,0 +1,13 @@
+package org.git;
+
+public class Course {
+	
+	public void employee() {
+		System.out.println("employee present");
+	}
+
+	public static void main(String[] args) {
+		Course c = new Course();
+		c.employee();
+	}
+}
